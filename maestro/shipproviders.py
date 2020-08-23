88975fd3b2175d631a2bef1a8cc14017bd14fc3c
Erwan Miran <mirwan666@gmail.com>
2020-08-23 12:51:51 +0200

    ability to define log_drivers available on ships

--- maestro/shipproviders.py
+++ maestro/shipproviders.py
@@ -55,7 +55,8 @@ class StaticShipsProvider(ShipsProvider):
                 tls_key=self._from_ship_or_defaults(v, 'tls_key'),
                 tls_verify=self._from_ship_or_defaults(v, 'tls_verify'),
                 tls_ca_cert=self._from_ship_or_defaults(v, 'tls_ca_cert'),
-                ssl_version=self._from_ship_or_defaults(v, 'ssl_version')))
+                ssl_version=self._from_ship_or_defaults(v, 'ssl_version'),
+                log_drivers=self._from_ship_or_defaults(v, 'log_drivers')))
             for k, v in self._config['ships'].items())
 
     def ships(self):
